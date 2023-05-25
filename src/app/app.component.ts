b947f7db67152049df7301d840a0b9e91b019664
William <william.silva@doutorfinancas.pt>
2023-05-25 13:50:42 +0100

    created step-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
-import { filter } from 'rxjs';
+import { FormGroup, FormControl, FormArray, Validators, AbstractControl, FormBuilder } from '@angular/forms';
+import { AppValidators } from './app.validators';
+import { filter, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,23 +10,34 @@ import { filter } from 'rxjs';
 })
 export class AppComponent {
   form:FormGroup = new FormGroup({
-    name: new FormControl(),
-    age: new FormControl(),
+    name: this.fb.control('', [Validators.required, Validators.minLength(3)]),
+    email: this.fb.control('', [], [AppValidators.checkDomainExist()]),
+    age: this.fb.control('', [Validators.required, AppValidators.checkIsNumber]),
     tel: new FormArray([
       new FormGroup({
-        code: new FormControl(),
-        number: new FormControl(),
+        code: this.fb.control(''),
+        number: this.fb.control(''),
       }),
     ]),
-  });
+  }, {validators: AppValidators.checkValidCountryNumber});
 
-  add(){
-    const tel = this.form.get('tel') as FormArray;
+  constructor(private fb: FormBuilder){
 
   }
 
+  add(){
+    const tel = this.form.get('tel') as FormArray;;
+    tel.push(
+      new FormGroup({
+        code: this.fb.control(''),
+        number: this.fb.control(''),
+      }),
+    );
+  }
+
   remove(index: number){
-    const tel = this.form.get('tel') as FormArray;
+    const tel = this.form.get('tel') as FormArray;;
+    tel.removeAt(index)
   }
 
   get tels(){
@@ -33,6 +45,30 @@ export class AppComponent {
   }
 
   reset(){
+    this.form.reset()
   }
+
+  ngOnInit(){
+    const email = this.form.get('email') as FormArray;
+    email.valueChanges.subscribe((value) => {
+      if(value === 'william.henrique@live.com'){
+        this.form.get('age')?.setValidators(Validators.min(20))
+      }else{
+        this.form.get('age')?.setValidators(Validators.min(30))
+      }
+      this.form.updateValueAndValidity();
+    })
+
+
+  }
+
+  require(key: string, errorKey: string){
+    return  this.form.get(`${key}`)?.hasError(`${errorKey}`) &&  this.form.get(`${key}`)?.dirty;
+  }
+
+
+
+
+
 
 }
